53fd04b6ec625f3a933007c41b82b21a3c1d2163
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-02-01 02:14:25 +0000

    Generates and identifies random id

--- app.js
+++ app.js
@@ -4,17 +4,47 @@ var app = express();
 var http = require("http").Server(app);
 var path = require("path");
 var io = require("socket.io")(http);
+var crypto = require("crypto");
 
 var config = require("./config");
+var users = {};
 
 app.use(bodyparser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-app.use(express.static(path.join(config.dirs.root, "client")));
+app.get("/:id", function(req, res){
+	var id = req.params.id;
+
+	if(typeof id === "undefined" || typeof users[id] === "undefined") {
+		res.status("404").send("Invalid identity");
+		return;
+	}
+
+	res.sendFile(path.join(config.dirs.root, "client","index.html"));
+});
+
 
 io.on("connection", function(socket) {
-	console.log("User connected..");
+	var id = "";
+
+	do {
+		id = randomString(5);
+	} while(typeof users[id] !== "undefined");
+
+	users[id] = {};
+
+	socket.emit("identify", { id:id });
+
+	console.log("User (" +id+ ") has connected");
+	socket.on("disconnect", function() {
+		console.log("Users (" +id+ ") has disconnected");
+		delete users[id];
+	});
 });
+
+function randomString(length) {
+	return Math.random().toString(36).substring(length);
+}
 
 http.listen(config.port, function() { console.log("listening on *:" + config.port); }); 
