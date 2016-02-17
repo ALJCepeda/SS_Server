8e42b9f7db8e56f1fecbaa142faf81e8063fed96
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-02-17 05:13:59 +0000

    Fixes config file

--- config.js
+++ config.js
@@ -1,11 +1,16 @@
-global.ONLINE = (process.argv.indexOf('--online') !== -1) ? true : false;
-global.DEV = (process.argv.indexOf('--dev') !== -1) ? true : false;
+global.LIVE = (process.argv.indexOf("--live") !== -1) ? true : false;
+global.DEV = (process.argv.indexOf("--dev") !== -1) ? true : false;
 
+var dirs = { root:"/shared/syncserver" };
+
+if(global.LIVE === true) {
+	dirs.root = "/var/live/syncserver";
+} else if(global.DEV === true) {
+	dirs.root = "/var/dev/syncserver";
+}
 module.exports = {
-	name: 'syncserver',
+	name: "Sync Server",
 	port: (global.DEV === true) ? 8804 : 8004,
 	apiKey: "09F7gtRWe![~S}1exnrf--gFDI1A44",
-	dirs: {
-		root: (global.ONLINE === true ) ? '/var/www/syncserver' : '/shared/syncserver'
-	}
+	dirs: dirs
 };
